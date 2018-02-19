8eba0a4b19683947357e4aa9c9fcd9ad23403897
transistor <golin.setup@gmail.com>
2018-02-19 13:53:54 -0300

    Initial commit

---
+++ app/src/main/java/com/sdg/setupservice/sem_cron_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sdg.setupservice.sem_cron_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
